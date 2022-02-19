4186860fded18b8c7a0d0219c944bce4c2ceef2c
agent-zero <0@x-cmd.com>
2022-02-19 03:13:37 +0000

    Squashed 'mod/gitee/' changes from ce8d3cb7..1cfea406
    1cfea406 update log and config load token
    
    git-subtree-dir: mod/gitee
    git-subtree-split: 1cfea40607066cbf16d4ee10bce23a3b98230780

--- .x-cmd/dev/test/gitee.testCase.sh
+++ .x-cmd/dev/test/gitee.testCase.sh
@@ -43,57 +43,57 @@ test_user_key(){
 }
 gt user key del --id "$(test_user_key_id)"
 gt user key add --key "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAxf6zWYFTpqAbMCrcbJZf5owaJUkiFWWulC8oRR+TOuQbnXJJW9OtItmk19kB3rKZKZerUSYNs82sud2vCBnY8yATBG4NkBOw0zkROl6FvehOuB9jeUJTMDJ4e1N203ar/mK2aSmku9QyPDdCy/yjzcqooPTnEHI6z40s0TdxoEkTxuPE4WkVLf2wBJsTvBCKGLroS1biX3kyQkMz6nhbLogv8EENfmJl1ibUpl1HL4fG2fMQbxrmfOdChSFw+qQEzKeQHwESEfgBBpdUt8fGHEqH0ijVU1aPuZlruvQhbHz+6ggK8Ri36HZ3IAWjHSkLnKf/VxeNF4W1Lbsm4sY5 tzw@tzw-G3-3579" --title "x_bash_test" 1> /dev/null 2>&1
-[ $? -ne 0 ] && ___x_cmd_gt_log error "gt member ls ERROR"
+[ $? -ne 0 ] && gt_log error "gt member ls ERROR"
 
 ## repo member
 gt member ls typeshell/dev 1> /dev/null 2>&1 1> /dev/null 2>&1
-[ $? -ne 0 ] && ___x_cmd_gt_log error "gt member ls ERROR"
+[ $? -ne 0 ] && gt_log error "gt member ls ERROR"
 gt member del -r typeshell/dev zhengqbbb 
 gt member add -r typeshell/dev -p push zhengqbbb niracler  1> /dev/null 2>&1
 niracler 1> /dev/null 2>&1
-[ $? -ne 0 ] && ___x_cmd_gt_log error "gt member del ERROR"[ $? -ne 0 ] && ___x_cmd_gt_log error "gt member add ERROR"
+[ $? -ne 0 ] && gt_log error "gt member del ERROR"[ $? -ne 0 ] && gt_log error "gt member add ERROR"
 
 
 ## enterprise
 ___x_cmd_gt_enterprise_test(){
     gt enterprise info  --admin 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "gt enterprise info ERROR"
+    [ $? -ne 0 ] && gt_log error "gt enterprise info ERROR"
     gt enterprise info  mycw_yx 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "gt enterprise info ERROR"
+    [ $? -ne 0 ] && gt_log error "gt enterprise info ERROR"
     gt enterprise repo ls  mycw_yx 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "gt enterprise repo  ERROR"
+    [ $? -ne 0 ] && gt_log error "gt enterprise repo  ERROR"
     gt enterprise member ls --enterprise lteam18 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "enterprise member ls  ERROR"
+    [ $? -ne 0 ] && gt_log error "enterprise member ls  ERROR"
     gt enterprise member user mycw_yx zhengqbbb 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "enterprise member user ERROR"
+    [ $? -ne 0 ] && gt_log error "enterprise member user ERROR"
     gt enterprise member del --enterprise mycw_yx zhengqbbb 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "enterprise member del ERROR"
+    [ $? -ne 0 ] && gt_log error "enterprise member del ERROR"
     gt enterprise member add -e mycw_yx --role member  zhengqbbb 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "enterprise member add ERROR"
+    [ $? -ne 0 ] && gt_log error "enterprise member add ERROR"
     gt enterprise member access  -e mycw_yx --username zhengqbbb --role admin --name 秋彬 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "enterprise member access ERROR"
-    ___x_cmd_gt_log info "enterprise 测试完成"
+    [ $? -ne 0 ] && gt_log error "enterprise member access ERROR"
+    gt_log info "enterprise 测试完成"
     return 0
 }
 
 ___x_cmd_gt_org_test(){
     gt org info 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org info ERROR"
+    [ $? -ne 0 ] && gt_log error "org info ERROR"
     gt org info strong_task 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org info ERROR"
+    [ $? -ne 0 ] && gt_log error "org info ERROR"
     gt org create x-bash-test 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org create ERROR" 
+    [ $? -ne 0 ] && gt_log error "org create ERROR" 
     gt org repo ls bash-gitee 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org repo ls ERROR" 
+    [ $? -ne 0 ] && gt_log error "org repo ls ERROR" 
     gt org member ls bash-gitee 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org member ls ERROR" 
+    [ $? -ne 0 ] && gt_log error "org member ls ERROR" 
     gt org member user --username mycw bash-gitee 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org repo ls ERROR" 
+    [ $? -ne 0 ] && gt_log error "org repo ls ERROR" 
     gt org member del --org bash-gitee zhengqbbb 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org member user ERROR" 
+    [ $? -ne 0 ] && gt_log error "org member user ERROR" 
     gt org member add --org bash-gitee zhengqbbb 1> /dev/null 2>&1
-    [ $? -ne 0 ] && ___x_cmd_gt_log error "org member add ERROR" 
-    ___x_cmd_gt_log info "org 测试完成"
+    [ $? -ne 0 ] && gt_log error "org member add ERROR" 
+    gt_log info "org 测试完成"
     return 0
 }
 
